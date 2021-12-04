6a870ba1ffa5cf5588c6c1ad0829362c2d3db074
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-12-04 16:16:35 +0100

    wrapping app in express server for render.com

--- src/index.js
+++ src/index.js
@@ -6,29 +6,41 @@ const CronJob = require('cron').CronJob;
 const Twit = require('twit');
 const axios = require('axios').default;
 
-const T = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true, // optional - requires SSL certificates to be valid.
-});
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
 
-var stream = T.stream('statuses/filter', { track: 'Chakra UI' });
+app.get('/', (req, res) =>
+  res.send("This is Chakra UI's Twitter -> Discord Bot!")
+);
 
-stream.on('tweet', function (tweet) {
-  axios.post(
-    process.env.DISCORD_WEBHOOK,
-    {
-      content: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
+app.listen(port, () => {
+  console.log(`Starting Twitter Stream...`);
+
+  const T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL: true, // optional - requires SSL certificates to be valid.
+  });
+
+  var stream = T.stream('statuses/filter', { track: 'Chakra UI' });
+
+  stream.on('tweet', function (tweet) {
+    axios.post(
+      process.env.DISCORD_WEBHOOK,
+      {
+        content: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
       },
-    }
-  );
-});
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  });
 
-console.log('Twitter Stream Started!');
+  console.log('Twitter Stream Started!');
+});
